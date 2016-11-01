dfb17c1a13dc9ccbd660cec3e59d8485619ea0e5
kjagoo <kagenyi2@gmail.com>
2016-11-01 11:06:30 +0300

    gets the max and min value of numbers on a list
    the function gets the highest value(max) and least value(min) of numbers in a string and outputs them in a list [min,max]

---
+++ maxmin.py
@@ -0,0 +1,14 @@
+
+def find_max_min(number):
+    
+    
+    max(number)
+    min(number)
+    if max(number)==min(number):
+        print [max(number)]
+    else :
+        print [ max(number),min(number)]
+
+find_max_min([1, 2, 3, 4])
+    
+
